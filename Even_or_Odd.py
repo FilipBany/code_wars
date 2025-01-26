d04d536e7abc422e7deda1784612a8c46a7eb916
FilipBany <148703994+FilipBany@users.noreply.github.com>
2025-01-26 20:08:25 +0100

    Update Even_or_Odd.py

--- Even_or_Odd.py
+++ Even_or_Odd.py
@@ -1,2 +1,8 @@
 def even_or_odd(number):
 	return 'Odd' if number % 2 else 'Even'
+
+print(even_or_odd(123456))
+print(even_or_odd(5657))
+print(even_or_odd(6578))
+print(even_or_odd(39))
+print(even_or_odd(2))
