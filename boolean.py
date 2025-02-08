5b4de32bfb2646f542c3230839fa55fe0ae4cd1d
FilipBany <148703994+FilipBany@users.noreply.github.com>
2025-02-08 12:18:19 +0100

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,10 @@
+def bool_to_word(boolean):
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
+
+print(bool_to_word(True))
+print(bool_to_word(False))
+print(bool_to_word(1))
+print(bool_to_word(0))
