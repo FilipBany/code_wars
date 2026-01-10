55b5863268be6588856b248ed16c46b31930527b
FilipBany <148703994+FilipBany@users.noreply.github.com>
2026-01-10 10:48:28 +0100

    gede.py

---
+++ grade.py
@@ -0,0 +1,8 @@
+def get_grade(s1, s2, s3):
+    score = (s1 + s2 + s3) / 3
+    
+    if score >= 90: return 'A'
+    if score >= 80: return 'B'
+    if score >= 70: return 'C'
+    if score >= 60: return 'D'
+    return 'F'
